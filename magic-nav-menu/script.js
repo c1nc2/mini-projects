0ce26b6e3c9f8612e881bb82c88a5f9b58e5233b
c1nc2 <127804466+c1nc2@users.noreply.github.com>
2023-07-24 14:13:51 +0900

    update

--- magic-nav-menu/script.js
+++ magic-nav-menu/script.js
@@ -1,2 +1,4 @@
 console.clear();
 const list = document.querySelectorAll('.list');
+const nav = document.querySelector('.navigation');
+list.forEach(item => item.addEventListener('click', function(e){
