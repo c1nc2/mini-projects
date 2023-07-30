e0d9ea6dae3755f3572c539269618d0b64728499
c1nc2 <127804466+c1nc2@users.noreply.github.com>
2023-07-31 08:24:58 +0900

    update

--- magic-nav-menu/script.js
+++ magic-nav-menu/script.js
@@ -4,3 +4,5 @@ const inputPassword = document.querySelector(".input__field");
 inputIcon.addEventListener("click", () => {
   inputIcon.classList.toggle("ri-eye-off-line");
   inputIcon.classList.toggle("ri-eye-line");
+  inputPassword.type = inputPassword.type === "password" ? "text" : "password";
+});
