607bbbabf66083fed7af588d05e5e857f405f082
c1nc2 <127804466+c1nc2@users.noreply.github.com>
2023-08-01 07:46:30 +0900

    update

--- magic-nav-menu/script.js
+++ magic-nav-menu/script.js
@@ -1,8 +1,23 @@
-const inputIcon = document.querySelector(".input__icon");
-const inputPassword = document.querySelector(".input__field");
-
-inputIcon.addEventListener("click", () => {
-  inputIcon.classList.toggle("ri-eye-off-line");
-  inputIcon.classList.toggle("ri-eye-line");
-  inputPassword.type = inputPassword.type === "password" ? "text" : "password";
-});
+function Strength(password) {
+    let i = 0;
+    if (password.length > 6) {
+      i++;
+    }
+    if (password.length >= 10) {
+      i++;
+    }
+  
+    if (/[A-Z]/.test(password)) {
+      i++;
+    }
+  
+    if (/[0-9]/.test(password)) {
+      i++;
+    }
+  
+    if (/[A-Za-z0-8]/.test(password)) {
+      i++;
+    }
+  
+    return i;
+  }
