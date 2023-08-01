40cf5c0582ec1542a47231e261017f2505e8a2c2
c1nc2 <127804466+c1nc2@users.noreply.github.com>
2023-08-02 00:11:35 +0900

    update

--- magic-nav-menu/script.js
+++ magic-nav-menu/script.js
@@ -21,3 +21,23 @@ function Strength(password) {
   
     return i;
   }
+  
+  let container = document.querySelector(".container");
+  document.addEventListener("keyup", function (e) {
+    let password = document.querySelector("#YourPassword").value;
+  
+    let strength = Strength(password);
+    if (strength <= 2) {
+      container.classList.add("weak");
+      container.classList.remove("moderate");
+      container.classList.remove("strong");
+    } else if (strength >= 2 && strength <= 4) {
+      container.classList.remove("weak");
+      container.classList.add("moderate");
+      container.classList.remove("strong");
+    } else {
+      container.classList.remove("weak");
+      container.classList.remove("moderate");
+      container.classList.add("strong");
+    }
+  });
